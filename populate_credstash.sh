e55c89636b056babf449378a940b2d8fb5099b45
Kerin Cosford <cosford@gmail.com>
2016-03-01 17:16:08 +0000

    Added simple credstash populate script

---
+++ populate_credstash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# exit if no env specified
+: ${1?"Usage: $0 ENVIRONMENT"}
+
+# make sure credstash table is present
+credstash -t ${1}-credentials setup
+
+# store each key/value pair
+cat ./aws_creds.env | while read line; do
+  IFS='=' read -r key value <<< "$line"
+  credstash -t ${1}-credentials put $key -a $value
+done
